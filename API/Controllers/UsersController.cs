e65674b292dc344d1f78d2431819d062bf074f2c
mahraz_hasnat <mahraz.ewu@gmail.com>
2025-01-11 16:12:14 +0600

    Added Authorization and Authientication to call the api.

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -7,6 +8,7 @@ using SQLitePCL;
 
 namespace API.Controllers
 {
+    [Authorize]
     public class UsersController(DataContext context) : BaseApiController
     {
         [HttpGet]
