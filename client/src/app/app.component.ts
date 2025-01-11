cba83104761a0d29f771c92ca5e8fa02fd5779a0
mahraz_hasnat <mahraz.ewu@gmail.com>
2025-01-11 13:16:20 +0600

    cleaned unused imports

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [NgFor, RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
