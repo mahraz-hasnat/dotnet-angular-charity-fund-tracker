0502663b334492ddb33a8c89c91ce57b1dd9c31b
mahraz_hasnat <mahraz.ewu@gmail.com>
2025-01-11 17:13:08 +0600

    added service extension for program.cs

--- API/Program.cs
+++ API/Program.cs
@@ -3,12 +3,10 @@ using API.Extensions;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddIdentityServices(builder.Configuration);
 
 var app = builder.Build();
-
 
 // Configure the HTTP request pipeline.
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
