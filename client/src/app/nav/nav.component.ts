1ad759737cf82fb954865ee27f2267d91fa080c6
mahraz_hasnat <mahraz.ewu@gmail.com>
2025-01-18 16:14:51 +0600

    Used ngx-bootstrap dropdown for user dropdown functionality

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { CommonModule } from '@angular/common';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, BsDropdownModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
