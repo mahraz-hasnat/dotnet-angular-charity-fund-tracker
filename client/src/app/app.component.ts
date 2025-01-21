acdf2e0008603b859a531eeac8a2573581b52003
mahraz_hasnat <mahraz.ewu@gmail.com>
2025-01-21 23:13:47 +0600

    Added home component in app component to include it in UI

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -5,13 +5,14 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './nav/nav.component';
 import { AccountService } from './_services/account.service';
 import { User } from './_models/user';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [NavComponent],
+  imports: [NavComponent, HomeComponent],
 })
 export class AppComponent implements OnInit {
   http = inject(HttpClient);
