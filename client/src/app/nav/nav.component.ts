5337d8385df0664d993e61431f5b817110e61786
mahraz_hasnat <mahraz.ewu@gmail.com>
2025-01-25 16:17:16 +0600

    imported router and routed commponents in navbar

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,11 +3,18 @@ import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, CommonModule, BsDropdownModule],
+  imports: [
+    FormsModule,
+    CommonModule,
+    BsDropdownModule,
+    RouterLink,
+    RouterLinkActive,
+  ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css',
 })
